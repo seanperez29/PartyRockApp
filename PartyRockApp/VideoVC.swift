17135f5a73e430d8096e1b4c2338a508babbdb57
Sean Perez <sean.perez29@gmail.com>
2017-03-11 20:52:11 -0700

    Add VideoVC with UIWebView

---
+++ PartyRockApp/VideoVC.swift
@@ -0,0 +1,19 @@
+//
+//  VideoVC.swift
+//  PartyRockApp
+//
+//  Created by Sean Perez on 3/11/17.
+//  Copyright © 2017 SeanPerez. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
