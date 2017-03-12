46911f2e228abcc097ddf2f3c078c5e17195fae9
Sean Perez <sean.perez29@gmail.com>
2017-03-11 20:49:33 -0700

    Design UI

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainVC.swift
+//  PartyRockApp
+//
+//  Created by Sean Perez on 3/11/17.
+//  Copyright © 2017 SeanPerez. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
